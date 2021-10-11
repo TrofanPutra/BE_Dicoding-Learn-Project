a3fa0ad90da91f4bd3daee280c114f4a227e5e3e
TrofanPutra <oumatrofan23@gmail.com>
2021-10-11 17:21:58 +0800

    adding response status to server.js

--- BE_Routing/server.js
+++ BE_Routing/server.js
@@ -4,18 +4,20 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
 
     const { method, url } = request;
 
     if (url === '/') { // penentu alamat website 
         if (method === 'GET') { // jika menggunakan method GET maka data yang akan di kembalikan seperti di bawah ini 
+            response.statusCode = 200; //response status 200 maka berhasil di akses 
             response.end('<h1>Selamat Datang pada HomePage</h1>');
         } else { // jika menggunakan method selain GET maka data yang akan di kembalikan seperti di bawah ini 
+            response.statusCode = 400; // response status 400 maka tidak berhasil di askses 
             response.end(`<h1>Halaman ini tidak dapat diakses dengan Method ${method} request</h1>`);
         }
     } else if (url === '/page') { // penentu alamat jika masuk di halaman "page"
         if (method === 'GET') { 
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman page</h1>')
         } else if (method === 'POST') { // jika menggunakan method POST maka data yang akan di post akan di olah seperti di bawah ini 
             let body = [];
@@ -27,12 +29,15 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString(); //data yang di ambil akan masuk ke dalam const "name"
                 const { name } = JSON.parse(body); //data di pindahkan kedalam 'name'
+                response.statusCode = 200;
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman page</h1>`); // hasil data yang di kembalikan untuk pengembalian method 
             });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman ini tidak dapat diakses dengan Method ${method} request</h1>`); // jika client menggunakan method selain GET dan POST maka data yang akan diterima akan menjadi seperti ini 
         }
     } else {
+        response.statusCode = 400;
         response.end('<h1>Halaman tidak ditemukan!</h1>'); // jika client memasukkan halaman diluar dari "/" dan "/page"
     }
 };
